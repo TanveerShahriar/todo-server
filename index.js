16f4073b0f05174f9d331843cf58ec2b46b0f95c
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-19 02:20:13 +0600

    fetching task done

--- index.js
+++ index.js
@@ -12,32 +12,42 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.31xws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
-  try {
-    await client.connect();
-    const tasksCollection = client.db("todo").collection("tasks");
+    try {
+        await client.connect();
+        const tasksCollection = client.db("todo").collection("tasks");
 
-    // Add
-    app.post("/tasks", async (req, res) => {
-        const task = req.body;
-        const result = await tasksCollection.insertOne(task);
-        res.send(result);
-      });
-  } finally {
-  }
+        // Add
+        app.post("/tasks", async (req, res) => {
+            const task = req.body;
+            const result = await tasksCollection.insertOne(task);
+            res.send(result);
+        });
+
+        // Get
+        app.get("/tasks", async (req, res) => {
+            const email = req.query.email;
+            const query = { email };
+            const cursor = tasksCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        });
+
+    } finally {
+    }
 }
 
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Running Server");
-  });
-  
-  app.listen(port, () => {
+});
+
+app.listen(port, () => {
     console.log("Listening to port", port);
-  });
+});
