8b47a75cb90c05f244bbcc0d2c477a3abbc25097
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-19 01:21:17 +0600

    task adding done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,30 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.31xws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const tasksCollection = client.db("todo").collection("tasks");
+
+    // Add
+    app.post("/tasks", async (req, res) => {
+        const task = req.body;
+        const result = await tasksCollection.insertOne(task);
+        res.send(result);
+      });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Running Server");
